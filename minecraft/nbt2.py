e6a542a642226555fdffe0aa687dd79ca48d5579
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2020-10-01 20:17:40 +0200

    New nbt handling
    NBT2 uses base classes to add a lot of functionality , and greatly simplify what was already there
    
    This version is not final, but it is stable

---
+++ minecraft/nbt2.py
@@ -0,0 +1,253 @@
+from abc import ABC, abstractmethod
+from collections.abc import MutableMapping, MutableSequence
+import functools
+import gzip
+import struct
+import zlib
+
+class TAG_Value(ABC):
+    """Base class for all Value tag types
+    Should NEVER be instanciated directly !
+    
+    assignments to .value are automatically boundary checked
+    """
+    
+    ID = NotImplemented
+    """TAG_ID of this Tag"""
+    
+    fmt = NotImplemented
+    """Format string for encoding and decoding"""
+    
+    snbt = NotImplemented
+    """SNBT value identifier"""
+
+    def __init__(self, value = 0):
+        self.value = value
+
+    def __int__(self):
+        return int(self.value)
+
+    def __float__(self):
+        return float(self.value)
+
+    def __add__(self, other):
+        return self.__class__(self.value + self.valueType(other))
+
+    def __sub__(self, other):
+        return self.__class__(self.value - self.valueType(other))
+
+    def __mul__(self, other):
+        return self.__class__(self.value * self.valueType(other))
+
+    def __truediv__(self, other):
+        return self.__class__(self.value / self.valueType(other))
+
+    def __floordiv__(self, other):
+        return self.__class__(self.value // self.valueType(other))
+
+    def __mod__(self, other):
+        return self.__class__(self.value % self.valueType(other))
+        
+    def __divmod__(self, other):
+        return(self // other, self % other)
+        
+    def __pow__(self, other):
+        return self.__class__(self.value ** self.valueType(other))
+    
+    def __repr__(self):
+        """Returns SNBT formatted tag"""
+        return f'{self.value}{self.snbt}'
+
+    @classmethod
+    def decode(cls, value):
+        """Convert bytes -> value for this TAG_ID"""
+        return struct.unpack(cls.fmt, value)[0]
+
+    @classmethod
+    def encode(cls, value):
+        """Convert value for this TAG_ID -> bytes"""
+        return struct.pack(cls.fmt, value)
+
+    @property
+    def value(self):
+        return self.decode(self._value)
+
+    @value.setter
+    def value(self, newValue):
+        newValue = self.valueType(newValue)
+        self._value = self.encode(newValue)
+
+    @staticmethod
+    @abstractmethod
+    def valueType(n):
+        pass
+
+class TAG_Byte(TAG_Value):
+    
+    ID = 1
+    fmt = 'B'
+    snbt = 'b'
+    valueType = int
+  
+class TAG_Short(TAG_Value):
+    
+    ID = 2
+    fmt = '>h'
+    snbt = 's'
+    valueType = int
+  
+class TAG_Int(TAG_Value):
+    
+    ID = 3
+    fmt = '>i'
+    snbt = ''
+    valueType = int
+
+class TAG_Long(TAG_Value):
+    
+    ID = 4
+    fmt = '>q'
+    snbt = 'l'
+    valueType = int
+ 
+class TAG_Float(TAG_Value):
+
+    ID = 5
+    fmt = '>f'
+    snbt = 'f'
+    valueType = float
+
+class TAG_Double(TAG_Value):
+    
+    ID = 6
+    fmt = '>d'
+    snbt = 'd'
+    valueType = float
+
+  
+class TAG_String():
+
+    ID = 8
+    
+    def encode(value):
+        value = str.encode(value)
+        
+        encoded = TAG_Short.encode(len(value))
+        encoded += value
+        
+        return encoded
+        
+    def decode(value):
+        return value[2:].decode(encoding='utf-8')
+
+
+class TAG_Compound(MutableMapping):
+    """A MutableMapping subclass with NBT specific functionality"""
+    
+    ID = 10
+
+    def __init__(self, payload):
+        self.payload = payload
+
+    def __iter__(self):
+        return iter(self.payload)
+
+    def __delitem__(self, key):
+        del self.payload[key]
+
+    def __getitem__(self, key):
+        return self.payload[key]
+    
+    def __setitem__(self, key, value):
+        self.payload[key] = value
+
+    def __len__(self):
+        return len(self.payload)
+
+    def __repr__(self):
+        snbt = '{'
+        # Recursive repr of contained tags
+        for i, element in enumerate(self.payload):
+            snbt += f'{element}:{self[element]}'
+            if i < len(self) - 1:
+                snbt += ','
+        snbt += '}'
+        return snbt
+
+    def encode(value):
+        encoded = bytearray()
+        for element in value:
+            # Recursively encode ID, Name, Payload
+            encoded += TAG_Byte.encode(value[element].ID)
+            encoded += TAG_String.encode(element)
+            encoded += type(value[element]).encode(value[element].payload)
+        encoded += (b'\x00')
+        return encoded
+        
+
+
+    
+class TAG_List(MutableSequence):
+    ID = 9
+    def __init__(self, payload = []):
+        self.payload = payload
+        
+    
+    def encode(value):
+        encoded = bytearray()
+        
+        encoded += TAG_Byte.encode(value[0].ID)
+        encoded += TAG_Int.encode(len(value))
+        
+        for element in value:
+            encoded += type(element).encode(element)
+            
+        return encoded
+        
+class TAG_Array(ABC, MutableSequence):
+    """Base class for all Array tag types
+    Should NEVER be instanciated directly !
+    """
+    
+    def __init__(self, payload):
+        self.payload = payload
+
+    def __getitem__(self, key):
+        return self.payload[key]
+        
+    def __setitem__(self, key, value):
+        if type(value) != self.elementID:
+            value = self.elementID(value)
+        self[key] = value
+        
+    def __delitem__(self, key):
+        del self[key]
+        
+    def __len__(self):
+        return len(self.payload)
+        
+    def insert(self, key, value):
+        self.payload = self[:key] + [value] + self[key:]
+
+    @classmethod
+    def encode(cls, value):
+        encoded = bytearray()
+        
+        encoded += TAG_Int.encode(len(value))
+        
+        for element in value:
+            encoded += cls.elementID.encode(element)
+            
+        return encoded
+
+class TAG_Byte_Array(TAG_Array):
+    ID = 7
+    elementID = TAG_Byte
+
+class TAG_Int_Array(TAG_Array):
+    ID = 11
+    elementID = TAG_Int
+    
+class TAG_Long_Array(TAG_Array):
+    ID = 12
+    elementID = TAG_Long
