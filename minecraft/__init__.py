6fe4347dd91338aa583918f0f2b8d496a73dada5
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2020-10-15 20:18:59 +0200

    Removed useless imports, added the new stuff in

--- minecraft/__init__.py
+++ minecraft/__init__.py
@@ -1,10 +1,4 @@
-import gzip
-import mmap
-import os
-import struct
-import time
-import zlib
-
 from .chunk import Chunk
 from .nbt import *
-from .old_datfile import DatFile
+from .datfile import DatFile
+from .compression import compress, decompress
