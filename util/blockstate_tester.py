48248300f959a96c37b4c54769edd1a619d74d1b
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-02-01 15:14:06 +0100

    Delete blockstate_tester.py
    Will be inside of blockaste.py

--- util/blockstate_tester.py
+++
@@ -1,8 +0,0 @@
-import minecraft
-
-testChunk = minecraft.Chunk.from_world(chunkX = 0, chunkZ = 0, world = 'debug')
-for x in range(16):
-    for y in range(256):
-        for z in range(16):
-            if testChunk.get_block(x,y,z)['Name'] == minecraft.TAG_String('minecraft:stone'):
-                print(str(x), str(y), str(z))
