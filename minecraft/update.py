c2467e97c8b511eb452399dc74a58d3fd232cc43
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-02-02 11:22:19 +0100

    New Blockstate Files !
    Auto generated by minecraft.update.blockstates() !

--- minecraft/update.py
+++ minecraft/update.py
@@ -1,45 +1,106 @@
 from minecraft.blockstate import BlockState
 from minecraft.chunk import Chunk
+import json
 
 def blockStates():
     """Update blockState files based on debug map"""
+    
     def create_property(value):
-        newProperty = {}
+        """Create a property dict fitting the given value"""
+        prop = {}
         if value in ['false', 'true']:
-            newProperty['type'] = 'bool'
+            prop['type'] = 'bool'
         elif value.isdigit():
-            newProperty['type'] = 'int'
-            newProperty['min'] = value
-            newProperty['max'] = value
+            prop['type'] = 'int'
+            prop['min'] = value
+            prop['max'] = value
         else:
-            newProperty['type'] = 'str'
-            newProperty['values'] = [value]
-        return newProperty
+            prop['type'] = 'str'
+            prop['values'] = [value]
+        return prop
     
-    def update_properties(block : BlockState):
-        """Update this block's file so that current properties are valid"""
-        valid = block.validProperties
-        for key, value in block['Properties'].items():
-            if key not in valid:
-                create_property(value)
-            else:
-                if valid[key]['type'] == 'bool':
-                    if value not in ['false', 'true']:
-                        raise ValueError(f'Found weird property {key} (was bool, can apparently also store {value})')
-                elif valid[key]['type'] == 'int':
-                    if not value.isdigit():
-                        raise ValueError(f'Found weird property{key} (was int, can apparently also store {value})')
-                    elif value < valid[key]['min']:
-                        valid[key]['min'] = value
-                    elif value > valid[key]['max']:
-                        valid[key]['max'] = value
-                elif valid[key]['type'] == 'str':
-                    valid[key]['values'].append(value)
+    def convert_property(prop : dict):
+        """Convert a property to str type
+        Used when a property appears to store multiple data types
+        """
+        if prop['type'] == 'bool':
+            values = ['false', 'true']
+        elif prop['type'] == 'int':
+            values = [i for i in range(prop['min'], prop['max'])]
+        else:
+            raise TypeError('Can only convert bool and int properties to str')
+        
+        return {'type':'str', 'values':values}
     
-    testChunk = Chunk.from_world(chunkX = 0, chunkZ = 0, world = 'debug')
-    for x in range(16):
-        for z in range(16):
-            testBlock = testChunk.get_block(x,70,z)
+    def update_property(prop: dict, value):
+        if prop['type'] == 'bool':
+            if value not in ['false', 'true']:
+                prop = convert_property(prop)
             
-
+        elif prop['type'] == 'int':
+            if not value.isdigit():
+                prop = convert_property(prop)
+            elif value < prop['min']:
+                prop['min'] = value
+            elif value > prop['max']:
+                prop['max'] = value
+    
+        if prop['type'] == 'str':
+            if value not in prop['values']:
+                prop['values'].append(value)
+        
+        return prop
+    
+    def update_block(block : BlockState):
+        """Update this block's file so that current properties are valid"""
+        try:
+            valid = block.validProperties
+        except FileNotFoundError:
+            valid = {}
+        
+        try:
+            for key, value in block['Properties'].items():
+                value = str(value)
+                if key in valid:
+                    valid[key] = update_property(valid[key], value)
+                else:
+                    valid[key] = create_property(value)
+        except KeyError:
+            pass
+        
+        with open(block.filePath, mode='w') as f:
+            json.dump(valid, f, indent = 4)
+    
+    chunkX = 0
+    chunkZ = 0
+    emptyChunks = 0
+    while True:
+    
+        testChunk = Chunk.from_world(chunkX, chunkZ, world = 'debug')
+        emptyBlocks = 0
+        print(f'Reading {testChunk}')
+        
+        for x in range(1,16,2):
+            for z in range(1,16,2):
+            
+                testBlock = testChunk.get_block(x,70,z)
+                
+                if testBlock['Name'] == 'minecraft:air':
+                    emptyBlocks += 1
+                
+                update_block(testBlock)
+        
+        if emptyBlocks >= 64:
+        
+            emptyChunks += 1
+            
+            if emptyChunks > 1:
+                break
+            else:
+                chunkX = 0
+                chunkZ += 1
+            
+        else:
+            chunkX += 1
+            emptyChunks = 0
     
