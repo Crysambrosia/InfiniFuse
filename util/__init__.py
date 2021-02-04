cd4d4de8b1400ccabc499cfa7e07fab7a79ad133
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-02-04 13:12:37 +0100

    Added get_bits
    Bits can be read and written to ! Separated common code into mask_and_shift

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,3 @@
 from .make_wrappers import make_wrappers
-from .binary import bits, read_bytes, get_bits
+from .binary import bitstr, read_bytes, get_bits, set_bits
 from .all_subclasses import all_subclasses
--- util/binary.py
+++ util/binary.py
@@ -1,6 +1,6 @@
-def bits(n, length : int = 0):
+def bitstr(n, length : int = 0):
     """Return num as <length> bits"""
-    return bin(n).removeprefix('0b').rjust(length, '0')
+    return bin(n).removeprefix('-').removeprefix('0b').rjust(length, '0')
 
 def read_bytes(iterable, n=0):
     """Read n bytes from the iterable and return them as a bytearray"""
@@ -19,12 +19,24 @@ def read_bytes(iterable, n=0):
     
     return value
     
-def bit_value(n, start, end, length=64):
-    """Read bits from <start> to <end> of a value, return as an int"""
-    mask = 2**(end-start)-1
-    shift = length - (end-start) - start
-    return (n & (mask << shift)) >> shift
+def get_bits(n, start, end, length : int = None):
+    """Read bits [<start>:<end>] of <n> and return them
+    <length> forces <n> into that bit length"""
+    mask, shift = mask_and_shift(start, end, length = n.bit_length() if length is None else length)
+    return (n & mask) >> shift
+
+def mask_and_shift(start, end, length):
+    """Return a mask and shift value to access [<start>:<end>] of <length> bits"""
+    shift = length - end
+    mask = (1 << end - start) - 1 << shift
+    return mask, shift
 
 def reverse(n):
     """Reverse the bitwise endianness of n"""
     return int(bin(n)[:1:-1], 2)
+
+def set_bits(n, start, end, newValue, length : int = None):
+    """Change bits [<start>:<end>] of <n> to <newValue> and return <n>
+    <length> forces <n> into that bit length"""
+    mask, shift = mask_and_shift(start, end, length = n.bit_length() if length is None else length)
+    return (n & ~mask) | (newValue << shift)
