d6dadc4680a8e56bd8a3dd395150547ca16cacc5
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-04-05 23:10:09 +0200

    Added map_test
    Creates a black and white image of chunk locations in said dimension. Also fixed incorrect loading and unloading

--- minecraft/dimension.py
+++ minecraft/dimension.py
@@ -1,4 +1,6 @@
+from .chunk import Chunk
 from .mcafile import McaFile
+from util.png import makePNG
 import os
 
 class Dimension():
@@ -35,7 +37,7 @@ class Dimension():
     
     def load(self, x : int, z : int):
         """Load chunk at <x> <y> to cache"""
-        self[(x, z)] = Chunk.open(folder = self.folder, x = x, z = z)
+        self[(x, z)] = McaFile.read_chunk(folder = self.folder, x = x, z = z)
     
     def load_all(self):
         """Load all chunks from this dimension
@@ -46,13 +48,71 @@ class Dimension():
         
             if os.path.splitext(fileName)[1] == '.mca':
             
-                file = McaFile(path = os.path.join(self.folder, fileName))
-                _, regionX, regionZ, _ = fileName.split('.')
-                print(f'Found region {regionX} {regionZ}')
+                path = os.path.join(self.folder, fileName)
+                with McaFile(path) as f:
+                    for chunk in f:
+                        if chunk is not None:
+                            coords = chunk.coords_chunk
+                            self[coords] = chunk
+    
+    def map_test(self):
+        """Create a pixel map of contained chunks within this dimension"""
+        
+        minX = 0
+        maxX = 0
+        minZ = 0
+        maxZ = 0
+        
+        all_coords = []
+        
+        for fileName in os.listdir(self.folder):
+            
+            print(f'Dealing with {fileName}...')
+            
+            if os.path.splitext(fileName)[1] == '.mca':
+            
+                path = os.path.join(self.folder, fileName)
+                
+                with McaFile(path) as f:
+                    for chunk in f:
+                        if chunk is not None:
+                        
+                            newCoords = chunk.coords_chunk
+                            all_coords.append(newCoords)
+                            
+                            if   newCoords[0] < minX:
+                                minX = newCoords[0]
+                            elif newCoords[0] > maxX:
+                                maxX = newCoords[0]
+                            
+                            if   newCoords[1] < minZ:
+                                minZ = newCoords[1]
+                            elif newCoords[1] > maxZ:
+                                maxZ = newCoords[1]
+        
+        width = abs(minX) + abs(maxX)
+        height = abs(minZ) + abs(maxZ)
+        
+        data = []
+        for z in range(height):
+        
+            row = []
+            
+            for x in range(width):
+            
+                if (minX + x, minZ + z) in all_coords:
+                    row.append(255)
+                else:
+                    row.append(0)
+            
+            data.append(row)
+        
+        with open(r'C:\Users\ambro\Documents\test.png', mode = 'w+b') as f:
+            f.write(makePNG(data))
     
     def unload(self, x : int, z : int):
         """Save chunk at <x> <z> and remove it from cache"""
-        self[(x, z)].save(folder = self.folder)
+        McaFile.write_chunk(folder = self.folder, value = self[(x, z)])
         del self[(x, z)]
     
     def unload_all(self):
@@ -61,5 +121,5 @@ class Dimension():
         # Copy keys because Python doesn't want the cache to change size during unloading
         
         for key in keys:
-            x, y, z = key
-            self.unload(x, y, z)
+            x, z = key
+            self.unload(x, z)
