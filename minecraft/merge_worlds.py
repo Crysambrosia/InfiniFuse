1563968f34399d4a10551243b58c72ffc077f648
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-04-07 02:06:57 +0200

    Started experimenting with offset finding
    Trying to see if chunk scale offsets are realistic to use

--- minecraft/merge_worlds.py
+++ minecraft/merge_worlds.py
@@ -1,3 +1,4 @@
+import minecraft
 import os
 
 # Find offset for map
@@ -10,9 +11,34 @@ import os
 
 def main():
 
-    appdata = os.environ['APPDATA']
-    saves = f'{appdata}\\.minecraft\\saves\\'
+    base = minecraft.World.from_saves('Test')
+    addition = minecraft.World.from_saves('Test2')
     
-    for world in os.listdir(saves):
-        worldFolder = os.path.join(saves, world)
-        
+    offsetX = 0
+    offsetZ = 0
+    minX, minZ, maxX, maxZ = base.dimensions['minecraft:overworld'].borders()
+    
+    for radius in range(3_750_000): # Using the 'radius' of a square of course
+        for offsetX in range(-radius, radius + 1):
+            for offsetZ in range(-radius, radius + 1):
+                if abs(offsetX) == radius or abs(offsetZ) == radius:
+                    print(f'trying offsets {offsetX} {offsetZ}')
+                    for x in range(minX, maxX):
+                        for z in range(minZ, maxZ):
+                            if (x, z) in addition.dimensions['minecraft:overworld'] and (x + offsetX, z + offsetZ) in base.dimensions['minecraft:overworld']:
+                                print(f'Conflict at {x} {z}')
+                                break
+                        else:
+                            continue
+                        break
+                    else:
+                        print(f'No conflict with offsets {offsetX} {offsetZ} !')
+                        break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
+    else:
+        print(f'No possible offset (you probably waited MONTHS to read this, sorry)')
