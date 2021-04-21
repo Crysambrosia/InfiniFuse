9638cabbcfa364f44a5c423b92783202bd8b8be6
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-04-21 17:54:53 +0200

    Started work on the real fuse function
    All update code for entities is done. Tile entities and etc still need to be updated

--- minecraft/merge_worlds.py
+++ minecraft/merge_worlds.py
@@ -11,10 +11,92 @@ import os
 # Offset players
 # Combine player stats
 # Do something with player inventory ?
-  
+
+def fuse(base : World, other : World):
+    """Fuse two maps into one. Takes a REALLY long time ! (Could be days)"""
+    
+    offset = find_offsets(a = base, b = other)
+    
+    netherXchunk, netherZchunk = find_offsets(a = base, b = other)
+    netherXblock = netherXchunk * 16
+    netherZblock = netherZchunk * 16
+    
+    overworldXchunk = netherXchunk * 8
+    overworldZchunk = netherZchunk * 8
+    
+    overworldXblock = overworldXchunk * 16
+    overworldZchunk = overworldZchunk * 16
+    
+    
+    for chunk in other.dimensions['minecraft:the_nether']:
+        chunk['']['Level']['xPos'] += netherXchunk
+        chunk['']['Level']['zPos'] += netherZchunk
+        
+        # Update all entity data that stores position
+        for i, entity in enumerate(chunk['']['Level']['Entities']):
+            entity['Pos'][0] += netherXblock
+            entity['Pos'][2] += netherZblock
+            
+            for key in entity['Brain']['memories']:
+                if key in [
+                    'minecraft:home',
+                    'minecraft:job_site',
+                    'minecraft:meeting_point',
+                    'minecraft:potential_job_site'
+                ]:
+                    memory = entity['Brain']['memories'][key]
+                
+                    if memory['value']['dimension'] == 'minecraft:the_nether':
+                        memory['value']['pos'][0] += netherXblock
+                        memory['value']['pos'][2] += netherZblock
+                        
+                    elif memory['value']['dimension'] == 'minecraft:overworld':
+                        memory['value']['pos'][0] += overworldXblock
+                        memory['value']['pos'][2] += overworldZblock
+                    
+                    entity['Brain']['memories'][key] = memory
+            
+            for key in entity:
+            
+                if key in [
+                    'BeamTarget'
+                    'FlowerPos',
+                    'HivePos',
+                    'Leash',
+                    'PatrolTarget',
+                    'WanderTarget'
+                ]:
+                    entity[key]['X'] += netherXblock
+                    entity[key]['Z'] += netherZblock
+                    
+                elif key in [
+                    'AX',
+                    'APX',
+                    'BoundX',
+                    'HomePosX',
+                    'SleepingX',
+                    'TileX',
+                    'TravelPosX',
+                    'TreasurePosX'
+                ]:
+                    entity[key] += netherXblock
+                    
+                elif key in [
+                    'AZ',
+                    'APZ',
+                    'BoundZ',
+                    'HomePosZ',
+                    'SleepingZ',
+                    'TileZ',
+                    'TravelPosZ',
+                    'TreasurePosZ'
+                ]:
+                    entity[key] += netherZblock
+            
+            chunk['']['Level']['Entities'][i] = entity
+
 def dimension_binary_map(dimension : Dimension):
     """Map all chunks from <dimension> to a two dimensional array of booleans"""
-    print(f'[{datetime.datetime.now()}] Making binary map of {dimension.folder}...')
     x = []
     z = []
     maps = {}
@@ -28,8 +110,6 @@ def dimension_binary_map(dimension : Dimension):
                 
                 regionX, regionZ = f.coords_region
                 maps[regionX, regionZ] = f.binary_map()
-    
-    print(f'[{datetime.datetime.now()}] Done !')
     return max(x), min(x), max(z), min(z), maps
     
 def generate_offsets(maxRadius : int = 3_750_000):
@@ -46,13 +126,19 @@ def generate_offsets(maxRadius : int = 3_750_000):
                 yield x,  radius
 
 def find_offsets(a : World, b : World):
-    """Fuse two maps into one. Takes a REALLY long time ! (Could be days)"""
+    """Find offsets with no conflicts to fuse the Overworld and Nether of <a> and <b>"""
     
+    print(f'[{datetime.datetime.now()}] Making binary map of base overworld...')
     aOverworld = dimension_binary_map(a.dimensions['minecraft:overworld'])
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of base nether...')
     aNether    = dimension_binary_map(a.dimensions['minecraft:the_nether'])
     # Binary maps of <a>'s overworld and nether
     
+    print(f'[{datetime.datetime.now()}] Making binary map of other overworld...')
     bOverworld = dimension_binary_map(b.dimensions['minecraft:overworld'])
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of other nether...')
     bNether    = dimension_binary_map(b.dimensions['minecraft:the_nether'])
     # Binary maps of <b>'s overworld and nether
     
