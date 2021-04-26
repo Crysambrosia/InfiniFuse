173b1176ca3758305bf00a5dc98cf6c318f1e0af
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-04-26 18:12:24 +0200

    Successfully transfered a chunk from one map to the other
    Made offset_chunk its own function, and fixed all the bugs I could find

--- minecraft/merge_worlds.py
+++ minecraft/merge_worlds.py
@@ -4,6 +4,7 @@ from .world import World
 import datetime
 import minecraft.TAG as TAG
 import os
+import random
 import util
 
 # Find offset for map
@@ -13,6 +14,55 @@ import util
 # Offset players
 # Combine player stats
 # Do something with player inventory ?
+
+def dimension_binary_map(dimension : Dimension):
+    """Map all chunks from <dimension> to a two dimensional array of booleans"""
+    x = []
+    z = []
+    maps = {}
+    
+    for fileName in os.listdir(dimension.folder):
+        if os.path.splitext(fileName)[1] == '.mca':
+            with McaFile(os.path.join(dimension.folder, fileName)) as f:
+                chunkX, chunkZ = f.coords_chunk
+                x += [chunkX, chunkX + 31]
+                z += [chunkZ, chunkZ + 31]
+                
+                regionX, regionZ = f.coords_region
+                maps[regionX, regionZ] = f.binary_map()
+    return max(x), min(x), max(z), min(z), maps
+
+def find_offsets(a : World, b : World):
+    """Find offsets with no conflicts to fuse the Overworld and Nether of <a> and <b>"""
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of base overworld...')
+    aOverworld = dimension_binary_map(a.dimensions['minecraft:overworld'])
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of base nether...')
+    aNether    = dimension_binary_map(a.dimensions['minecraft:the_nether'])
+    # Binary maps of <a>'s overworld and nether
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of other overworld...')
+    bOverworld = dimension_binary_map(b.dimensions['minecraft:overworld'])
+    
+    print(f'[{datetime.datetime.now()}] Making binary map of other nether...')
+    bNether    = dimension_binary_map(b.dimensions['minecraft:the_nether'])
+    # Binary maps of <b>'s overworld and nether
+    
+    print(f'[{datetime.datetime.now()}] Trying offsets...')
+    for netherOffset in generate_offsets():
+        
+        # Finding offset for the nether first makes the process faster
+        # The nether is usually 8 times smaller than the overworld, and roughly the same shape
+        # Thus, there is a very high chance a given nether offset will work for the overworld too
+        # And it will be up to 8 times quicker to find !
+        
+        if not offset_conflicts(a = aNether, b = bNether, offset = netherOffset):
+            overworldOffset = tuple([i*8 for i in netherOffset])
+            if not offset_conflicts(a = aOverworld, b = bOverworld, offset = overworldOffset):
+            
+                print(f'[{datetime.datetime.now()}] Found offset : {netherOffset} for the Nether, {overworldOffset} for the overworld !')
+                return netherOffset
 
 def fuse(base : World, other : World):
     """Fuse two maps into one. Takes a REALLY long time ! (Could be days)"""
@@ -199,28 +249,7 @@ def fuse(base : World, other : World):
                                 start['Processed'][i] = process
                         
                         chunk['']['Level']['Structures']['Starts'][startKey] = start
-        
 
-        # Update PostProcessing
-
-
-def dimension_binary_map(dimension : Dimension):
-    """Map all chunks from <dimension> to a two dimensional array of booleans"""
-    x = []
-    z = []
-    maps = {}
-    
-    for fileName in os.listdir(dimension.folder):
-        if os.path.splitext(fileName)[1] == '.mca':
-            with McaFile(os.path.join(dimension.folder, fileName)) as f:
-                chunkX, chunkZ = f.coords_chunk
-                x += [chunkX, chunkX + 31]
-                z += [chunkZ, chunkZ + 31]
-                
-                regionX, regionZ = f.coords_region
-                maps[regionX, regionZ] = f.binary_map()
-    return max(x), min(x), max(z), min(z), maps
-    
 def generate_offsets(maxRadius : int = 3_750_000):
     """Generate x and z coordinates in concentric squares around the origin"""
     for radius in range(maxRadius):
@@ -234,37 +263,185 @@ def generate_offsets(maxRadius : int = 3_750_000):
                 yield x, -radius
                 yield x,  radius
 
-def find_offsets(a : World, b : World):
-    """Find offsets with no conflicts to fuse the Overworld and Nether of <a> and <b>"""
+def offset_chunk(chunk, offsetXchunk, offsetZchunk):
+    """Offset a chunk by <offsetX> <offsetZ> chunks on the grid"""
+    offsetXblock = offsetXchunk * 16
+    offsetZblock = offsetZchunk * 16
     
-    print(f'[{datetime.datetime.now()}] Making binary map of base overworld...')
-    aOverworld = dimension_binary_map(a.dimensions['minecraft:overworld'])
+    chunk['']['Level']['xPos'] += offsetXchunk
+    chunk['']['Level']['zPos'] += offsetZchunk
     
-    print(f'[{datetime.datetime.now()}] Making binary map of base nether...')
-    aNether    = dimension_binary_map(a.dimensions['minecraft:the_nether'])
-    # Binary maps of <a>'s overworld and nether
-    
-    print(f'[{datetime.datetime.now()}] Making binary map of other overworld...')
-    bOverworld = dimension_binary_map(b.dimensions['minecraft:overworld'])
-    
-    print(f'[{datetime.datetime.now()}] Making binary map of other nether...')
-    bNether    = dimension_binary_map(b.dimensions['minecraft:the_nether'])
-    # Binary maps of <b>'s overworld and nether
-    
-    print(f'[{datetime.datetime.now()}] Trying offsets...')
-    for netherOffset in generate_offsets():
-        
-        # Finding offset for the nether first makes the process faster
-        # The nether is usually 8 times smaller than the overworld, and roughly the same shape
-        # Thus, there is a very high chance a given nether offset will work for the overworld too
-        # And it will be up to 8 times quicker to find !
-        
-        if not offset_conflicts(a = aNether, b = bNether, offset = netherOffset):
-            overworldOffset = tuple([i*8 for i in netherOffset])
-            if not offset_conflicts(a = aOverworld, b = bOverworld, offset = overworldOffset):
+    # Update all entity data that stores position
+    if 'Entities' in chunk['']['Level']:
+        for i, entity in enumerate(chunk['']['Level']['Entities']):
+            entity['Pos'][0] += offsetXblock
+            entity['Pos'][2] += offsetZblock
             
-                print(f'[{datetime.datetime.now()}] Found offset : {netherOffset} for the Nether, {overworldOffset} for the overworld !')
-                return netherOffset
+            for idx, _ in enumerate(entity['UUID']):
+                entity['UUID'][idx] = TAG.Int(random.randint(-2_147_483_648, 2_147_483_647))
+            
+            for key in entity:
+            
+                if key == 'Brain':
+                    for memKey in entity['Brain']['memories']:
+                        if memKey in [
+                            'minecraft:home',
+                            'minecraft:job_site',
+                            'minecraft:meeting_point',
+                            'minecraft:potential_job_site'
+                        ]:
+                            memory = entity['Brain']['memories'][memKey]
+                        
+                            # This doesn't take into account memory['value']['dimension']
+                            # Since villagers become disconnected when changing dimensions,
+                            # either this tag is not present when the villager is in another dimension
+                            # or it will be deleted by the game next time the villager is updated
+                            memory['value']['pos'][0] += offsetXblock
+                            memory['value']['pos'][2] += offsetZblock
+                            
+                            entity['Brain']['memories'][memKey] = memory
+                    
+            
+                if key in [
+                    'BeamTarget'
+                    'FlowerPos',
+                    'HivePos',
+                    'Leash',
+                    'PatrolTarget',
+                    'WanderTarget'
+                ]:
+                    entity[key]['X'] += offsetXblock
+                    entity[key]['Z'] += offsetZblock
+                    
+                elif key in [
+                    'AX',
+                    'APX',
+                    'BoundX',
+                    'HomePosX',
+                    'SleepingX',
+                    'TileX',
+                    'TravelPosX',
+                    'TreasurePosX'
+                ]:
+                    entity[key] += offsetXblock
+                    
+                elif key in [
+                    'AZ',
+                    'APZ',
+                    'BoundZ',
+                    'HomePosZ',
+                    'SleepingZ',
+                    'TileZ',
+                    'TravelPosZ',
+                    'TreasurePosZ'
+                ]:
+                    entity[key] += offsetZblock
+            
+            chunk['']['Level']['Entities'][i] = entity
+   
+    # Update tile entities
+    # This does NOT update map IDs yet !
+    if 'TileEntities' in chunk['']['Level']:
+        for i, entity in enumerate(chunk['']['Level']['TileEntities']):
+            entity['x'] += offsetXblock
+            entity['z'] += offsetZblock
+            
+            for key in entity:
+                if key in ['ExitPortal', 'FlowerPos']:
+                    entity[key]['X'] += offsetXblock
+                    entity[key]['Z'] += offsetZblock
+            
+            chunk['']['Level']['TileEntities'][i] = entity
+    
+    # Update TileTicks
+    if 'TileTicks' in chunk['']['Level']:
+        for i, tick in enumerate(chunk['']['Level']['TileTicks']):
+            tick['x'] += offsetXblock
+            tick['z'] += offsetZblock
+        
+            chunk['']['Level']['TileTicks'][i] = tick
+    
+    # Update LiquidTicks
+    if 'LiquidTicks' in chunk['']['Level']:
+        for i, tick in enumerate(chunk['']['Level']['LiquidTicks']):
+            tick['x'] += offsetXblock
+            tick['z'] += offsetZblock
+        
+            chunk['']['Level']['LiquidTicks'][i] = tick
+    
+    # Update Structures
+    if 'Structures' in chunk['']['Level']:
+    
+        # Update References
+        if 'References' in chunk['']['Level']['Structures']:
+            for key, reference in chunk['']['Level']['Structures']['References'].items():
+                if reference != []:
+                    for i, coords in enumerate(reference):
+                        x = TAG.Int()
+                        z = TAG.Int()
+                        
+                        x.unsigned = util.get_bits(coords, 0, 32)  + offsetXchunk
+                        z.unsigned = util.get_bits(coords, 32, 64) + offsetZchunk
+                        
+                        coords.unsigned = util.set_bits(coords, 0, 32, x)
+                        coords.unsigned = util.set_bits(coords, 32, 64, z)
+                        
+                        reference[i] = coords
+                    
+                    chunk['']['Level']['Structures']['References'][key] = reference
+        
+        # Update Starts
+        if 'Starts' in chunk['']['Level']['Structures']:
+            for key, start in chunk['']['Level']['Structures']['Starts'].items():
+                if start['id'] != 'INVALID':
+                    
+                    start['BB'][0] += offsetXblock
+                    start['BB'][2] += offsetZblock
+                    start['BB'][3] += offsetXblock
+                    start['BB'][5] += offsetZblock
+                    
+                    start['ChunkX'] += offsetXchunk
+                    start['ChunkZ'] += offsetZchunk
+                    
+                    if 'Children' in start:
+                        for i, child in enumerate(start['Children']):
+                            child['BB'][0] += offsetXblock
+                            child['BB'][2] += offsetZblock
+                            child['BB'][3] += offsetXblock
+                            child['BB'][5] += offsetZblock
+                            
+                            for key in child:
+                                if key == 'Entrances':
+                                    for i in child['Entrances']:
+                                        child['Entrances'][i][0] += offsetXblock
+                                        child['Entrances'][i][2] += offsetZblock
+                                        child['Entrances'][i][3] += offsetXblock
+                                        child['Entrances'][i][5] += offsetZblock
+                                    
+                                elif key == 'junctions':
+                                    for iJunction, junction in enumerate(child['junctions']):
+                                        junction['source_x'] += offsetXblock
+                                        junction['source_z'] += offsetZblock
+                                        
+                                        child['junctions'][iJunction] = junction
+                                    
+                                elif key in ['PosX', 'TPX']:
+                                    child[key] += offsetXblock
+                                    
+                                elif key in ['PosZ', 'TPZ']:
+                                    child[key] += offsetZblock
+                            
+                            start['Children'][i] = child
+                    
+                    if 'Processed' in start:
+                        for i, process in enumerate(start['Processed']):
+                            process['X'] += offsetXchunk
+                            process['Z'] += offsetZchunk
+                            start['Processed'][i] = process
+                    
+                    chunk['']['Level']['Structures']['Starts'][startKey] = start
+    
+    return chunk
 
 def offset_conflicts(a, b, offset):
     """Check for conflicts if <b> was fused into <a> at <offset>"""
