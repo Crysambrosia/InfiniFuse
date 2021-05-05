79b751b4978f5c170b82bc98e2537dcb7dc9dbfd
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-05-05 16:04:05 +0200

    Basic stats and playerdata read/write
    Thinking about changing how DatFile and McaFile behave so they are more like json...

--- minecraft/world/player.py
+++ minecraft/world/player.py
@@ -1,2 +1,29 @@
+from minecraft.datfile import DatFile
+import json
+import minecraft.TAG as TAG
+import os
+
 class Player():
-    pass
+    """Handles accessing and modifying a player's data and stats"""
+    def __init__(self, uuid : str, folder : str):
+        """UUID must be in the hyphenated-hexadecimal format"""
+        self.folder = folder
+        self.uuid = uuid
+        
+        statsPath = os.path.join(self.folder, 'stats', f'{self.uuid}.json')
+        with open(statsPath, mode = 'r') as f:
+            self.stats = json.load(f)
+        
+        dataPath = os.path.join(self.folder, 'playerdata', f'{self.uuid}.dat')
+        self.playerdata = DatFile.open(path = dataPath)
+    
+    def write(self):
+        """Write changes to file"""
+        statsPath = os.path.join(self.folder, 'stats', f'{self.uuid}.json')
+        with open(path, mode = 'w') as f:
+            json.dump(self.stats, f)
+        
+        self.playerdata.write()
+    
+    
+    
