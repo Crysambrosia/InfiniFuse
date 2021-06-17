260179a80a8aa77ff7703dd89d2ebd28b0877ee3
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-06-17 23:01:34 +0200

    Added PNG to import list

--- util/__init__.py
+++ util/__init__.py
@@ -2,4 +2,4 @@ from .all_subclasses import all_subclasses
 from .binary import bitstr, read_bytes, get_bits, set_bits
 from .cache import Cache
 from .make_wrappers import make_wrappers
-from .png import makePNG
+from .png import makePNG, PNG
