feb1b17f8b7d965ce00f432ea9f459dd5e8ed088
AmyLucyRose <71721186+AmyLucyRose@users.noreply.github.com>
2021-06-17 23:53:41 +0200

    Added png_maps
    Makes chunk maps of all default dimensions !

--- minecraft/world/world.py
+++ minecraft/world/world.py
@@ -1,6 +1,7 @@
 from .dimension import Dimension
 from .mapmanager import MapManager
 from .playermanager import PlayerManager
+import logging
 import os
 
 class World():
@@ -26,3 +27,31 @@ class World():
         appdata = os.environ['APPDATA']
         folder = os.path.join(appdata, '.minecraft', 'saves', name)
         return cls(folder)
+    
+    def png_maps(self, folder : str = None, size : int = 256, skipEnd : bool = False):
+        """Make PNG maps of all dimensions of this world. Each pixel will represent a chunk.
+        
+        <folder>  : path to folder in which to save the PNGs. Defaults to this world's folder
+        <size>    : Side length of the nether map will be the closest multiple of 32 to this
+                    Overworld will be 8 times larger to cover the same area
+                    Defaults to 256
+        <skipEnd> : Whether the end dimension should be skipped, defaults to False
+        """
+        folder = folder or self.folder
+        
+        for dimensionName, dimension in self.dimensions.items():
+            if skipEnd and dimensionName == 'minecraft:the_end':
+                continue
+            
+            if dimensionName == 'minecraft:overworld':
+                actualSize = size * 8
+            else:
+                actualSize = size
+            
+            path = os.path.join(folder, f'{dimensionName.split(":")[1]}_map.png')
+            
+            logging.info(f'Making map of {dimensionName}...')
+            with open(path, mode = 'wb') as f:
+                f.write(dimension.png_map(size = actualSize).content())
+        
+        logging.info('Done !')
